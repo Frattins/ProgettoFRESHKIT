7902610811ab502ce413c201562376cc18333d44
Frattins <davide.frattini1@gmail.com>
2024-09-18 19:43:15 +0200

    ok

--- JerseyShop/Controllers/FavoritesController.cs
+++ JerseyShop/Controllers/FavoritesController.cs
@@ -5,7 +5,9 @@ using JerseyShop.Data;
 using JerseyShop.Models;
 using System.Threading.Tasks;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 
+[Authorize]
 public class FavoritesController : Controller
 {
     private readonly FootballShopContext _context;
@@ -18,29 +20,41 @@ public class FavoritesController : Controller
     }
 
     [HttpPost]
-    [HttpPost]
     public async Task<IActionResult> ToggleFavorite(int magliaId)
     {
         var userId = _userManager.GetUserId(User);
+        if (userId == null)
+        {
+            return Unauthorized();
+        }
+
         var favorite = await _context.Favorites
             .FirstOrDefaultAsync(f => f.UserId == userId && f.MagliaId == magliaId);
 
+        bool isFavorite;
         if (favorite == null)
         {
             _context.Favorites.Add(new Favorite { UserId = userId, MagliaId = magliaId });
+            isFavorite = true;
         }
         else
         {
             _context.Favorites.Remove(favorite);
+            isFavorite = false;
         }
 
         await _context.SaveChangesAsync();
-        return Json(new { isFavorite = favorite == null });
+        return Json(new { isFavorite = isFavorite });
     }
 
     public async Task<IActionResult> Index()
     {
         var userId = _userManager.GetUserId(User);
+        if (userId == null)
+        {
+            return Unauthorized();
+        }
+
         var favorites = await _context.Favorites
             .Where(f => f.UserId == userId)
             .Include(f => f.Maglia)
